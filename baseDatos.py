230a4d7aeb1c07ef2e0d333efed24f88d0606e49
kristell <213476@ids.upchiapas.edu.mx>
2023-10-26 16:12:00 -0600

    second commit: add sp and view

--- baseDatos.py
+++ baseDatos.py
@@ -22,7 +22,6 @@ cursor.execute('''
         producto_id INT,
         cantidad INT,
         fecha DATE,
-        INDEX fk_producto_id (producto_id),
         FOREIGN KEY (producto_id) REFERENCES productos(id)
     )
 ''')
@@ -33,7 +32,6 @@ cursor.execute('''
         producto_id INT,
         cantidad INT,
         fecha DATE,
-        INDEX fk_producto_id (producto_id),
         FOREIGN KEY (producto_id) REFERENCES productos(id)
     )
 ''')
@@ -43,15 +41,48 @@ cursor.execute('''
         id INT AUTO_INCREMENT PRIMARY KEY,
         producto_id INT,
         cantidad INT,
-        INDEX fk_producto_id (producto_id),
         FOREIGN KEY (producto_id) REFERENCES productos(id)
     )
 ''')
 
+#Sp
+cursor.execute('''
+CREATE PROCEDURE sp_insertar_entrada(
+    IN p_producto_id INT,
+    IN p_cantidad INT,
+    IN p_fecha DATE
+)
+BEGIN
+    INSERT INTO entrada (producto_id, cantidad, fecha)
+    VALUES (p_producto_id, p_cantidad, p_fecha);
+END
+''')
+cursor.execute('''
+CREATE PROCEDURE sp_actualizar_entrada(
+    IN p_entrada_id INT,
+    IN p_producto_id INT,
+    IN p_cantidad INT,
+    IN p_fecha DATE
+)
+BEGIN
+    UPDATE entrada
+    SET producto_id = p_producto_id, cantidad = p_cantidad, fecha = p_fecha
+    WHERE id = p_entrada_id;
+END
+''')
 
-
+# Crear vista inventario_productos
+cursor.execute('''
+CREATE VIEW vista_inventario_productos AS
+SELECT p.id AS producto_id, p.nombre AS producto_nombre, p.precio AS producto_precio, i.cantidad AS inventario_cantidad
+FROM productos p
+LEFT JOIN inventario i ON p.id = i.producto_id;
+''')
 
 conexion.commit()
 conexion.close()
 
 print("Tablas creadas exitosamente en la base de datos MySQL.")
+
+
+
